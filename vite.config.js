13f92e3ca77e9022db3f83cf280045248a5a933e
micrv <your.email@example.com>
2025-03-04 21:41:52 +0000

    Fix asset loading by explicitly copying assets in workflow and enabling publicDir

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,8 @@ export default defineConfig({
                 },
                 format: 'es'
             }
-        }
+        },
+        copyPublicDir: true
     },
     publicDir: 'assets',
     resolve: {
